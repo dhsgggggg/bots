7d904da2454ec7221d2bd083f0066b015639dbfd
dhsgggggg <136965949+dhsgggggg@users.noreply.github.com>
2023-07-02 17:45:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Replace YOUR_BOT_TOKEN with your actual bot token
-updater = Updater(token=' 5707101777:AAH2ttEJTJCv0YcnATqsY_zrsD0IdSNd5e4 ', use_context=True)
+updater = Updater(token='YOUR_BOT_TOKEN', use_context=True)
 
 def time(update, context):
     import datetime
@@ -9,19 +9,27 @@ def time(update, context):
     update.message.reply_text('The current time is {}'.format(now))
 
 time_handler = MessageHandler(Filters.regex(r'(\btime\b|\bوقت\b)'), time)
-dispatcher.add_handler(time_handler)
+updater.dispatcher.add_handler(time_handler)
 
 def start(update, context):
     update.message.reply_text('Bot started')
 
 start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+updater.dispatcher.add_handler(start_handler)
+
+def handle_command(update, context):
+    command = update.message.text.split()[0].lower()
+    if command == '/time':
+        time(update, context)
+
+command_handler = MessageHandler(Filters.command, handle_command)
+updater.dispatcher.add_handler(command_handler)
 
 def error(update, context):
     print(f"Update {update} caused error {context.error}")
     update.message.reply_text("Sorry, I couldn't process your message. Please try again later.")
 
-dispatcher.add_error_handler(error)
+updater.dispatcher.add_error_handler(error)
 
 updater.start_polling()
 updater.idle()
